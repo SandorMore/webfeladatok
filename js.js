91068c298b54157ffc97d539bde10067ece78887
Your Name <you@example.com>
2024-11-25 08:22:52 +0100

    commit

--- js.js
+++ js.js
@@ -1,1 +1,6 @@
 const buttonEl = document.getElementById("submit")
+const containerEl = document.getElementById("markolo")
+function buttonClicked(){
+    let newelement = document.createElement("div")
+    containerEl.appendChild(newelement)
+}
