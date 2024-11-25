9e310cb2791b38e38eb8e47b70a84ba0ec365604
Your Name <you@example.com>
2024-11-25 08:41:57 +0100

    sigma sigma on the wall whos the skibidest of them all

--- js.js
+++ js.js
@@ -4,7 +4,8 @@ const inputEL = document.getElementById("input")
 function buttonClicked(){
     let newelement = document.createElement("div")
     let newelement2 = document.createElement("h1")
-    newelement2.innerText = inputEL.innerText
+    newelement2.innerText = inputEL.value
     markoloEl.appendChild(newelement)
     newelement.appendChild(newelement2)
+
 }   
