3fa71fbef08793c6f7d962e281c9b72ef93e85e8
Your Name <you@example.com>
2024-12-02 13:28:43 +0100

    cigan

--- quiz/script.js
+++ quiz/script.js
@@ -1,6 +1,23 @@
 const API_URL = "http://127.0.0.1:5500/quiz/api.json"
 
-let a = "alma"
+const gameRootEl = document.getElementById("gameRoot")
+
+
+
 fetch(API_URL)
 .then(valasz => valasz.json())
-.then(atalakitott => console.log(atalakitott.results[0]))
+.then(atalakitott => {
+    console.log(atalakitott.results[0])
+})
+
+function generateQuestion(question){
+    let questionText = document.createElement("h1")
+    questionText.innerHTML = question.question
+
+    let correct = question.correct_answer
+    let incorrect = question.incorrect_answer
+    // deconstruction
+    let answers = [correct, ...incorrect]
+
+
+}
